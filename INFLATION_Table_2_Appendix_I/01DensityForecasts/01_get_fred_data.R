57222091ed764abd7afa3254c7cbf291b14bc42c
rdpunder <rdpunder@dpunder.com>
2025-09-17 10:26:44 +0200

    New folder structure

---
+++ INFLATION_Table_2_Appendix_I/01DensityForecasts/01_get_fred_data.R
@@ -0,0 +1,41 @@
+# must install FRED package
+#library(devtools)
+#install_github("cykbennie/fbi")
+
+library(fbi)
+library(tidyverse)
+library(TTR)
+
+start_date = as.Date("1960-01-01")
+end_date = as.Date("2015-12-01")
+start_date_min1 = as.Date("1959-12-01")
+
+#data = fredmd("https://files.stlouisfed.org/files/htdocs/fred-md/monthly/current.csv")
+#data_raw = fredmd("https://files.stlouisfed.org/files/htdocs/fred-md/monthly/current.csv",
+#                  transform = FALSE)
+
+data  <- fredmd("Data/2025-08-md.csv", date_start = start_date_min1, date_end = end_date)
+data_raw  <- fredmd("Data/2025-08-md.csv", date_start = start_date_min1, date_end = end_date, transform = FALSE)
+
+varlist = fredmd_description
+vars = intersect(colnames(data),varlist$fred)
+
+data = data %>% as.tibble()%>%
+  select(all_of(c("date",vars)))
+varlist = varlist%>%filter(fred%in%vars)
+prices_varlist = varlist%>%filter(group=="Prices",tcode==6)
+
+data = data%>% as_tibble()%>%
+  select( -all_of(prices_varlist$fred) )
+
+prices = data_raw %>% as_tibble() %>%
+  select(all_of(prices_varlist$fred))%>%
+  mutate_all(.funs = function(x)100*c(NA,x%>%log()%>%diff()))
+
+data = cbind(data%>%as.data.frame(),prices%>%as.data.frame())
+
+data = data %>%
+  filter(date>=start_date)%>%
+  select_if(~ !any(is.na(.)))
+
+save(data,file = "data/data.rda")
